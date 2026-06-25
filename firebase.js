2c22da300c6e962e95891efa28b532c11ad33870
EstiloHub <lau382132@gmail.com>
2026-06-25 18:16:03 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -6,25 +6,11 @@ import {
 } from
   "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import {
+  getFirestore
 } from
   "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-const firebaseConfig= {
-  apikey: "AIzaSyAa3tscObg263CLdVyrUN7ZrB4LbO-1JgQ",
-  authDomain: "estilo-hub.firebaseapp.com",
-  projectId: "estilo-hub",
-  storangeBucket: "estilo-hub.firebasestorange.app",
-  messagingSenderId: "442111984908",
-  appId: "1.442111984908:web:28aa9a2ca682c3174b3529"
-};
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+ const firebaseConfig = {
     apiKey: "AIzaSyAa3tscObg263CLdVyrUN7ZrB4LbO-1JgQ",
     authDomain: "estilo-hub.firebaseapp.com",
     projectId: "estilo-hub",
@@ -33,11 +19,6 @@ const firebaseConfig= {
     appId: "1:442111984908:web:28aa9a2ca682c3174b3529"
   };
 
-
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
 const app= initializeApp(firebaseConfig);
 
 const auth= getAuth(app);
